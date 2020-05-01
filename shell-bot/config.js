748a6405628869683a14173ae2d95ea4c08d0bd1
Alfred-fcj <64628152+Alfred-fcj@users.noreply.github.com>
2020-05-01 05:29:04 +0000

    Update config.js

--- shell-bot/config.js
+++ shell-bot/config.js
@@ -2,8 +2,8 @@ module.exports = {
     config:function(){
         return (
             {
-                adminUsers:'799912166,799416438', //admin users' telegram id, should be numbers
-                botToken: '1201585296:AAHT3gPFDyFNz5fOVXpm8c3A9YWLYN_PFkc', // bot token
+                adminUsers:'Chat_id', //admin users' telegram id, should be numbers
+                botToken: '[API_TOKEN]', // bot token
 
             }
         );
