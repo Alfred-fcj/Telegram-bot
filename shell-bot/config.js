abb066e3d15ef6f2c60cf447a60d7800123d40fb
Alfred-fcj <64628152+Alfred-fcj@users.noreply.github.com>
2020-05-01 05:21:34 +0000

    Add files via upload

---
+++ shell-bot/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    config:function(){
+        return (
+            {
+                adminUsers:'799912166,799416438', //admin users' telegram id, should be numbers
+                botToken: '1201585296:AAHT3gPFDyFNz5fOVXpm8c3A9YWLYN_PFkc', // bot token
+
+            }
+        );
+    }
+};
